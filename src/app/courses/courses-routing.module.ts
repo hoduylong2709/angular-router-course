a4f0ff7985fae94de7729ff4c5e3b417babbb8dd
Long Ho <long_ho@datahouse.com>
2023-04-27 14:13:36 +0700

    longho - 1st commit

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,22 +1,42 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { CourseComponent } from "./course/course.component";
+import { CourseResolver } from "./services/course.resolver";
+import { LessonDetailComponent } from "./lesson/lesson-detail.component";
+import { LessonsListComponent } from "./lessons-list/lessons-list.component";
+import { LessonsResolver } from "./services/lessons.resolver";
 
 const routes: Routes = [
-
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: ":courseUrl",
+    component: CourseComponent,
+    children: [
+      {
+        path: "",
+        component: LessonsListComponent,
+        resolve: {
+          lessons: LessonsResolver,
+        },
+      },
+      {
+        path: "lessons/:lessonSeqNo",
+        component: LessonDetailComponent,
+      },
+    ],
+    resolve: {
+      course: CourseResolver,
+    },
+  },
 ];
 
 @NgModule({
-  imports: [
-
-  ],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  providers: [
-
-  ]
+  providers: [CourseResolver, LessonsResolver],
 })
-export class CoursesRoutingModule {
-
-
-
-}
+export class CoursesRoutingModule {}
